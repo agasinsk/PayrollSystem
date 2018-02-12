479069a4960b654ee81ea76a993109044d1cd075
Artur Gasiński <arturgasinski@hotmail.com>
2018-02-12 19:43:08 +0100

    Added ChangeEmployeeTransaction base class

---
+++ SalaryRCM/Transactions/Employee/ChangeEmployeeTransaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PayrollSystem.Transactions.Employee
+{
+    public abstract class ChangeEmployeeTransaction : BaseTransaction
+    {
+        private readonly int employeeId;
+
+        protected ChangeEmployeeTransaction(int employeeId)
+        {
+            this.employeeId = employeeId;
+        }
+
+        public override void Execute()
+        {
+            var employee = payrollDatabase.GetEmployee(employeeId);
+            if (employee == null)
+            {
+                throw new ApplicationException($"Employee of id {employeeId} cannot be found");
+            }
+            Change(employee);
+        }
+
+        protected abstract void Change(Models.Employee employee);
+    }
+}
---
+++ SalaryRCMTests/ChangeEmployeeTransactionsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayrollSystem;
+using PayrollSystem.PaymentClassifications;
+using PayrollSystem.PaymentMethods;
+using PayrollSystem.PaymentSchedules;
+using PayrollSystem.Transactions.Employee;
+
+namespace PayrollSystemTests
+{
+    [TestClass]
+    public class ChangeEmployeeTransactionsTests
+    {
+        private PayrollDatabase payrollDatabase;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            payrollDatabase = PayrollDatabase.GetInstance();
+        }
+
+        [TestMethod]
+        public void TestChangeNameTransaction()
+        {
+            // Arrange
+            var employeeId = 1;
+            var employeeName = "Bogdan";
+            var employeeAddress = "Address";
+            var hourlyRate = 2500M;
+
+            new AddHourlyEmployeeTransaction(employeeId, employeeName, employeeAddress, hourlyRate).Execute();
+
+            const string newName = "Bartosz";
+
+            // Act
+            new ChangeEmployeeNameTransaction(employeeId, newName).Execute();
+            var employee = payrollDatabase.GetEmployee(employeeId);
+
+            // Assert
+            Assert.IsNotNull(employee);
+            Assert.AreEqual(newName, employee.Name);
+        }
+    }
+}
